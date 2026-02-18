bd4cdec8c46c3ee25d3eefcf3ca695756b668449
Mihagan12pro <mikhail.kargin.05@mail.ru>
2026-02-18 10:17:19 +0300

    feat: add new struct - matrix

---
+++ src/Models/Matrix/Matrix.cs
@@ -0,0 +1,16 @@
+﻿namespace Models.Matrix
+{
+    public struct Matrix<T>
+    {
+        public readonly int RowsCount;
+        public readonly int ColumnsCount;
+
+        private readonly T[][] _matrix;
+
+        public Matrix(int rowsCount, int columnsCount)
+        {
+            RowsCount = rowsCount;
+            ColumnsCount = columnsCount;
+        }
+    }
+}
